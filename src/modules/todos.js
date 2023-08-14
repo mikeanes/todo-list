a2038f65422995af770a15e9c83aa62d4a4e7926
Michael <m.anestopoulos24@gmail.com>
2023-08-14 15:11:26 -0400

    added data retreival functions in todo

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,5 +1,23 @@
-const Todo = (title, description, dueDate, priority, checklist) => {
-    return{title, description, dueDate, priority, checklist};
+const Todo = (title, description, dueDate, priority) => {
+
+    let _title = title;
+    let _description = description;
+    let _dueDate = dueDate;
+    let _priority = priority;
+
+    function getTitle(){
+        return _title;
+    };
+    function getDescription(){
+        return _description;
+    };
+    function getDueDate(){
+        return _dueDate;
+    };
+    function getPriority(){
+        return _priority;
+    };
+    return{getTitle, getDescription, getDueDate, getPriority};
 };
 
 export default Todo;
